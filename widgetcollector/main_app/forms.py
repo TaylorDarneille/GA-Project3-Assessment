6f59e465ba78af84d3a1c756057559cb4224bcf4
Taylor Darneille <tmdarneille@gmail.com>
2020-05-17 19:50:01 -0700

    added ModelForm note in forms.py

--- widgetcollector/main_app/forms.py
+++ widgetcollector/main_app/forms.py
@@ -1,2 +1,7 @@
 from django.forms import ModelForm
 from .models import Widget
+
+# You are correct that this is needed to render the add widget form on the home page, but you didn't actually define the ModelForm
+# class WidgetForm(ModelForm):
+#     class Meta:
+#         model = Widget
--- widgetcollector/main_app/models.py
+++ widgetcollector/main_app/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class Widget(models.Model):
     description = models.CharField(max_length=100)
+    # missing parentheses on the .IntegerField method below
+    # see docs: https://docs.djangoproject.com/en/3.0/topics/db/models/#fields
+    # quantity = models.IntegerField(default=1)
     quantity = models.IntegerField
